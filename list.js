d0a19971d02bf44a0d43968399563b66f09f9305
todoom1 <aa13976383@outlook.com>
2026-02-23 16:06:03 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,44 @@
+
+document.getElementById("yh").addEventListener("click", function() {
+  window.open(atob("aHR0cHM6Ly9nZnZneC5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD04MDAmcHQ9NUJFMkI0QkQtNDE2OS1DNkRGLTZDQTMtMDE5OEM1QTA4NDQx"), '_blank');
+});
+document.getElementById("ff").addEventListener("click", function() {
+  window.open(atob("aHR0cHM6Ly9sbmNxYy5qZ3F2amkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDUwJnRvcElkPTQwMTE3NzE3NjY2ODkxNjImc2VsZlBsYW5JZD00MjY2MjQ5"), '_blank');
+});
+document.getElementById("cf").addEventListener("click", function() {
+  window.open(atob("aHR0cHM6Ly9wa2FqaW0uaGZqY2wuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDMyJnRvcElkPTIyOTg0DQ=="), '_blank');
+});
+document.getElementById("st").addEventListener("click", function() {
+  window.open(atob("aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTIwNzI5ODgmc2VsZlBsYW5JZD0xNjY1ODA3"), '_blank');
+});
+document.getElementById("xsd").addEventListener("click", function() {
+  window.open(atob("aHR0cHM6Ly9kZXJlYXIuZm5icHN3LmNvbS9hcHAvcmVnaXN0ZXIucGhwP3NpdGVfaWQ9MTA1OSZ0b3BJZD0xNTg5MzI2JnNlbGZQbGFuSWQ9MTI3NTAyNA"), '_blank');
+});
+document.getElementById("jl").addEventListener("click", function() {
+  window.open(atob("aHR0cHM6Ly90ZmFqbi5oY2p3ai5jb20vYXBwL3JlZ2lzdGVyLnBocD9zaXRlX2lkPTIwODEmdG9wSWQ9MTY4MTQzMCZzZWxmUGxhbklkPTE1NTkwMTg"), '_blank');
+});
+document.getElementById("yhn").innerHTML="壹号娱乐";
+document.getElementById("ffn").innerHTML="非凡娱乐";
+document.getElementById("cfn").innerHTML="超凡国际";
+document.getElementById("stn").innerHTML="胜天国际";
+document.getElementById("xsdn").innerHTML="新时代";
+document.getElementById("jln").innerHTML="君临国际";
+document.getElementById("2").innerHTML="🔥十年信誉平台推荐";
+document.getElementById("1").innerHTML="N66E.MY";
+const texts = ["祝您新年快乐！", "马到成功", "N66E.MY"];
+const today = new Date();
+const month = today.getMonth() + 1;
+const year = today.getFullYear();
+const date = today.getDate();
+let currentIndex = 0;
+
+if (year === 2026 && month === 2 && date >= 17 && date <= 23) {
+    alert("🎇祝您新年快乐！🎇"); 
+setInterval(() => {
+document.getElementById("1").innerHTML = texts[currentIndex];
+currentIndex = (currentIndex + 1) % texts.length;
+}, 2026);
+
+} else if (year === 2026 && month === 3 && date === 3) {
+    alert("🎇祝您元宵节快乐！🎇");
+}
