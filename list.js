147ea11d779df15c8a5d4d4cae6c9beb273852f5
todoom1 <aa13976383@outlook.com>
2026-04-08 20:54:06 +0800

    Update list.js

--- list.js
+++ list.js
@@ -37,21 +37,6 @@ document.getElementById("xsdn").innerHTML="新时代";
 document.getElementById("jln").innerHTML="君临国际";
 document.getElementById("2").innerHTML="🔥十年信誉平台推荐";
 document.getElementById("1").innerHTML="N66E.MY";
-const texts = ["祝您新年快乐！", "马到成功", "N66E.MY"];
-const today = new Date();
-const month = today.getMonth() + 1;
-const year = today.getFullYear();
-const date = today.getDate();
-let currentIndex = 0;
-
-if (year === 2026 && month === 2 && date >= 17 && date <= 23) {
-    alert("🎇祝您新年快乐！🎇"); 
-setInterval(() => {
-document.getElementById("1").innerHTML = texts[currentIndex];
-currentIndex = (currentIndex + 1) % texts.length;
-}, 2026);
-
-} else if (year === 2026 && month === 3 && date === 3) {
-    alert("🎇祝您元宵节快乐！🎇");
-}
-
+window.onload = function() {
+      alert("欢迎访问N66E！");
+    };
